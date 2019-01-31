89d1ed7c485fa9d71236ce8eea8f44f8fde0b6bd
Luiz Felipe <23662020+lffg@users.noreply.github.com>
2019-01-30 23:19:45 -0200

    fix: fix logout mutation

--- src/app/modules/User/LogoutResolver.ts
+++ src/app/modules/User/LogoutResolver.ts
@@ -3,11 +3,12 @@ import { MyContext } from '../../../types/MyContext'
 
 @Resolver()
 export class LogoutResolver {
-  @Mutation(() => Boolean)
+  @Mutation(() => Boolean, { nullable: true })
   async logout(
     @Ctx()
     ctx: MyContext
-  ): Promise<boolean> {
-    return new Promise((res) => ctx.req.session!.destroy((err) => res(!!err)))
+  ): Promise<boolean | null> {
+    if (!ctx.req.session!.userId) return null
+    return new Promise((res) => ctx.req.session!.destroy((err) => res(!err)))
   }
 }
